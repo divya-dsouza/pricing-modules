d264ed1e9a3fd9c632d34d832dd779c2167f552c
divya-dsouza <pramjithadivya@gmail.com>
2023-07-05 11:25:05 +0530

    Added Spring cloud gateway feature with Google auth

--- pricing-modules/sql-dataaccess/src/main/java/org/divyad/repository/FileUploadRepository.java
+++
@@ -1,8 +0,0 @@
-package org.divyad.repository;
-
-import org.divyad.domain.FileUpload;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileUploadRepository extends JpaRepository<FileUpload, Long> {
-}
-
